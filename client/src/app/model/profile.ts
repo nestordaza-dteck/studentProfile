f0a26c86f4ed5861cc6a219555434f3bfcd34c34
charles-augustin <charlesjebalitherson@gmail.com>
2020-05-29 00:12:45 -0400

    Profile Model | Shared Modules

---
+++ client/src/app/model/profile.ts
@@ -0,0 +1,11 @@
+export class Profile {
+    studentId: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    dob: Date;
+    currentStatus: string;
+    description: string;
+    address: string;
+}
